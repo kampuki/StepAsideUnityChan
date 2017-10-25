54ad933a0e12330b3caaab36b3487796a76c853e
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-10-25 21:16:07 +0900

    アイテム削除
    アイテム削除をしようとしてもエラーが出ます。どうすればいいですか？

---
+++ Assets/MYcameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MYcameraController : MonoBehaviour {
+
+	private GameObject unitychan;
+
+	private float differance;
+
+	// Use this for initialization
+	void Start () {
+
+		this.unitychan = GameObject.Find ("unitychan");
+
+		this.differance = unitychan.transform.position.z - this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = new Vector3 (0, this.transform.position.y, this.unitychan.transform.position.z - differance);
+	}
+}
