a42155071e1f19be015f96dca63cb0af662d5313
ruki hamaguchi <king-king-7@ezweb.ne.jp>
2017-10-27 11:44:19 +0900

    アイテムの削除
    DestroyWallを使ってアイテムを削除できるようにしました。

---
+++ Assets/DestroyItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour {
+
+	public Transform charactor;
+
+	public GameObject carprefab;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (charactor.position.z > carprefab.transform.position.z) {
+
+			Destroy (carprefab);
+	}
+}
+}
---
+++ Assets/DestroyTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTrigger : MonoBehaviour {
+
+	private GameObject unityChan;
+
+	private float difference;
+
+	// Use this for initialization
+	void Start () {
+
+		this.unityChan = GameObject.Find ("unitychan");
+
+		this.difference = unityChan.transform.position.z - this.transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.position = new Vector3 (0, this.transform.position.y, this.unityChan.transform.position.z - difference);
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+
+		if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag") {
+
+			Destroy (other.gameObject);
+
+		}
+	}
+}
